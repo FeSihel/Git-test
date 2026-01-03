36e54c1ac8585037c8a6b56a81330aee943c2df1
FeSihel <felipesihel@gmail.com>
2026-01-03 15:45:18 -0300

    new feature

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 print("Hello from git bash! \n I did some changes")
 a = "this was created on website!"
-b = 6 #merge conflict by b being 5 or 6 (desktop change) or 7 (webiste change)
+b = 6.5 #merge conflict by b being 5 or 6 (desktop change) or 7 (webiste change)
+
+c = int(input()) #working on feature branch
+d = c % 2
+if d == 0:
+    print("even")
+else:
+    print("odd")
